a83cb19a151c36e2325f14fe5ef9d3a2db61c416
erikosampaio <erikoa.93@gmail.com>
2022-07-06 10:30:30 -0300

    Ajustando o sistema de layouts do site (Início)

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-class WelcomeController < ApplicationController
-  # Caso queira colocar uma autenticação básica para login
-  # http_basic_authenticate_with name: "eriko", password: "123"
-  
-  def index
-  end
-end
