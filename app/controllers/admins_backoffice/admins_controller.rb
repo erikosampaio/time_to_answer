7f44ccd4d1cfbba45fa9fd57535f19079d49efba
erikosampaio <erikoa.93@gmail.com>
2022-07-25 11:53:55 -0300

    Crio método para deletar administrador

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,6 +1,6 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController  
   before_action :password_verify, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
   
   def index
     @admins = Admin.all
@@ -13,7 +13,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def create
     @admin = Admin.new(params_admin)
     if @admin.save
-      redirect_to admins_backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi criado com sucesso!"
+      redirect_to admins_backoffice_admins_path, notice: "Administrador criado com sucesso!"
     else
       render :new
     end
@@ -24,9 +24,17 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
-      redirect_to admins_backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi atualizado com sucesso!"
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador deletado com sucesso!"
+    else
+      render :index
     end
   end
 
