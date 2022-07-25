058a722242c527a8a68fd2e9faa84397db81e92f
erikosampaio <erikoa.93@gmail.com>
2022-07-25 20:36:45 -0300

    Crio controller para as Subjects

---
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -0,0 +1,49 @@
+class AdminsBackoffice::SubjectsController < AdminsBackofficeController
+  before_action :set_admin, only: [:edit, :update, :destroy]
+  
+  def index
+    @subject = Subject.all.page params[:page]
+  end
+
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(params_subject)
+    if @subject.save
+      redirect_to admins_backoffice_subject_path, notice: "Assunto/Área criado com sucesso!"
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if @subject.update(params_subject)
+      redirect_to admins_backoffice_subject_path, notice: "Assunto/Área atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @subject.destroy
+      redirect_to admins_backoffice_subject_path, notice: "Assunto/Área deletado com sucesso!"
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def params_admin
+    params.require(:subject).permit(:description)
+  end
+
+  def set_admin
+    @subject = Subject.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
     resources :admins # Adminstradores
+    resources :subjects # Assuntos/Áreas
   end
 
   devise_for :admins
