6b2b4298a8faf92508acf8aa35bd08e8f9becd0b
erikosampaio <erikoa.93@gmail.com>
2022-07-27 16:16:04 -0300

    Incremento I18n para os models

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,6 @@
 module AdminsBackofficeHelper
+
+  def translate_attributes(object = nil, method = nil)
+    object.present? && method.present? ? object.model.human_attribute_name(method) : "Informe os parâmetros corretamente"
+  end
 end
