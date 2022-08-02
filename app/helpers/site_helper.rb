489edad2f11f0b7fa1c5a2faf088688e461d573d
erikosampaio <erikoa.93@gmail.com>
2022-08-02 13:31:25 -0300

    Filtrando questões por assunto

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas..."
+    when 'questions'
+      "Resultados para \"#{params[:term]}\"..."
+    when 'subject'
+      "Questões sobre o assunto \"#{params[:subject]}\"..."
+    end
+  end
 end
