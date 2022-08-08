cd8a02d5eed8bec4c8afa7b915785b01498c02bb
erikosampaio <erikoa.93@gmail.com>
2022-08-08 17:29:41 -0300

    Adiciono radio button para gênero + função javascript

--- app/helpers/users_backoffice/profile_helper.rb
+++ app/helpers/users_backoffice/profile_helper.rb
@@ -11,4 +11,8 @@ module UsersBackoffice::ProfileHelper
   def name_user_present
     "#{current_user.full_name}!"
   end
+
+  def gender_select(user, current_gender)
+    user.user_profile.gender == current_gender ? 'btn-primary' : 'btn-default'
+  end
 end
