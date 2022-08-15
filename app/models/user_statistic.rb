5a6559030ec5bbd7d24b405fc9f021d6ea85b251
erikosampaio <erikoa.93@gmail.com>
2022-08-15 17:42:29 -0300

    Refatoro o UserStatistic levando o código para o model

--- app/models/user_statistic.rb
+++ app/models/user_statistic.rb
@@ -5,4 +5,15 @@ class UserStatistic < ApplicationRecord
   def total_questions
     self.right_questions + self.wrong_questions
   end
+
+  # Class method
+  def self.set_statistic(answer, current_user)
+    # Se o usuário NÃO ESTIVER logado, current_user é nil. Com um ! ele se tornaria true, e com mais um ! ele se tornaria em false.
+    # Se o usuário ESTIVER logado, current_user é o usuário logado. Com um ! ele se tornaria false, e com mais um ! ele se tornaria true.
+    if !!current_user
+      user_statistic = UserStatistic.find_or_create_by(user: current_user)
+      answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
+      user_statistic.save
+    end
+  end
 end
