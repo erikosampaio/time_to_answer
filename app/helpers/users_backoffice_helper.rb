ef6cd8bd91d6893aa3b5b9f00b4ea60aec41f142
erikosampaio <erikoa.93@gmail.com>
2022-08-15 15:27:06 -0300

    Corrijo nome do arquivo para jpg e adiciono nome e sobrenome na task de criação de usuário padrão

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,6 +1,6 @@
 module UsersBackofficeHelper
 
   def avatar_url
-    current_user.user_profile.avatar.attached? ? current_user.user_profile.avatar : "img.png"
+    current_user.user_profile.avatar.attached? ? current_user.user_profile.avatar : "img.jpg"
   end
 end
