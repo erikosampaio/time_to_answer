36a64de83b4caf14a6dc9249af9d1dd33747bdba
erikosampaio <erikoa.93@gmail.com>
2022-08-18 15:35:56 -0300

    Corrigo JS para mensagens no layout user devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   # Devise Config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Mailtrap Config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'ad77f12816cc74',
+    :password => '340bc84ebb56f9',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
