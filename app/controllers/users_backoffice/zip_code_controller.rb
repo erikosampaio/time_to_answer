52b8d02eb9aff79e737b99c4c1ea7a3799b15353
erikosampaio <erikoa.93@gmail.com>
2022-10-27 16:57:33 -0300

    Configuro busca do CEP com api VIACEP com controller e view

---
+++ app/controllers/users_backoffice/zip_code_controller.rb
@@ -0,0 +1,6 @@
+class UsersBackoffice::ZipCodeController < UsersBackofficeController
+
+  def show
+    @cep = Cep.new(params[:zip_code])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_15_211532) do
+ActiveRecord::Schema.define(version: 2022_10_27_192217) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2022_08_15_211532) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
